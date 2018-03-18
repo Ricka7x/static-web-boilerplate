7f9267aa572f6f6f0b9be608043ce361f9f8bbed
ricka7x <ricky.ramirez89@gmail.com>
2018-03-17 18:19:31 -0600

    added development server

--- tasks/start.js
+++ tasks/start.js
@@ -1,3 +1,12 @@
 const gulp = require('gulp')
+const bs = require('browser-sync').create()
+const routes = require('../routes')
 
-gulp.task('start', ['templates', 'styles', 'scripts'])
+gulp.task('start', ['templates', 'styles', 'scripts'], () =>
+  bs.init({
+    notify: false,
+    server: {
+      baseDir: routes.dev
+    }
+  })
+)
