897c5b7bfacee45416f578ee755e5695555bea4a
ricka7x <ricky.ramirez89@gmail.com>
2018-05-03 12:45:01 -0500

    switched from gulp-ruby-sass to gulp-sass

--- tasks/styles.js
+++ tasks/styles.js
@@ -2,13 +2,19 @@ const gulp = require('gulp')
 const plumber = require('gulp-plumber')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
-const sass = require('gulp-ruby-sass')
+const sass = require('gulp-sass')
 const { src, dev, assets } = require('../config')
 
 gulp.task('styles', () =>
-  sass(`${src}/${assets}/styles/main.scss`, { style: 'expanded' })
+  gulp
+    .src(`${src}/${assets}/styles/main.scss`)
     .pipe(plumber())
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+        includePaths: ['node_modules']
+      }).on('error', sass.logError)
+    )
     .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
-    .on('error', err => console.log(err)) // eslint-disable-line  no-console
     .pipe(gulp.dest(`${dev}/${assets}/styles`))
 )
